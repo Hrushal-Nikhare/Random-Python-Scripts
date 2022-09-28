ee269e5486263952ac458f6cc6d0019afa344a5f
Hrushal Nikhare <Hrushalnikhare@gmail.com>
2022-09-28 20:50:02 +0530

    Update factorial.py

--- School/factorial.py
+++ School/factorial.py
@@ -3,3 +3,8 @@ import math
 n = int(input("Number: "))
 
 print(math.factorial(n))
+# for negative number error
+#try: 
+ #   print(math.factorial(n))
+#except:
+ #   print("factorial for negative numbers dont exist")
