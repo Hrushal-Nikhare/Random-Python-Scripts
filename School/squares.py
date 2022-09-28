937fdf46331f50bbf752f7fbb538a34abb15fb4c
Hrushal Nikhare <Hrushalnikhare@gmail.com>
2022-09-28 20:57:34 +0530

    Update squares.py
    Simplify it cause bean brains

--- School/squares.py
+++ School/squares.py
@@ -1,5 +1,7 @@
 # 9.	Python Program for Sum of squares of first n natural numbers. 
 n = int(input("Number: "))
 
-sum1 = sum(i**2 for i in range(1, n+1))
+sum1 = 0
+for i in range(0,n+1):
+    sum1 += i**2
 print(sum1)
